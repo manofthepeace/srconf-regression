8a5632628b5dc36c042396707b9ff2cd382b9bd6
Alex Martel <13215031+manofthepeace@users.noreply.github.com>
2025-03-26 10:58:06 -0400

    Initial Commit

---
+++ src/main/java/org/acme/MyConfig.java
@@ -0,0 +1,37 @@
+package org.acme;
+
+import java.util.List;
+import java.util.Map;
+
+import io.quarkus.runtime.annotations.StaticInitSafe;
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+import io.smallrye.config.WithParentName;
+
+@StaticInitSafe
+@ConfigMapping(prefix = "my")
+public interface MyConfig {
+
+    @WithName("config.main")
+    List<MyMapping> sources();
+
+    @WithName("config.urls")
+    Map<Integer, String> urls();
+
+    @WithParentName
+    Map<String, Config> types();
+
+    interface MyMapping {
+        String key();
+
+        List<String> id();
+    }
+
+    public interface Config {
+
+        int maxConcurrentStart();
+
+        int maxConcurrentStop();
+    }
+
+}
